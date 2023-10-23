9dee4d65155b8e7335ad90c6858c82f4acc2057f
wolffbruno <wolffbruno72@gmail.com>
2023-10-23 20:57:21 -0300

    feat(package.json): add new dependencies for date picker and popover components feat(date-picker.tsx): create new DatePickerWithRange component for date range selection feat(calendar.tsx): create new Calendar component for date selection feat(popover.tsx): create new Popover component for date picker popover feat(useVehicleTelemetry.ts): add new hook useVehicleTelemetryHistoryFrames for fetching telemetry data within a date range feat(frame-list.tsx): integrate DatePickerWithRange component and useVehicleTelemetryHistoryFrames hook to filter frames by date range

--- src/hooks/useVehicleTelemetry.ts
+++ src/hooks/useVehicleTelemetry.ts
@@ -1,28 +1,54 @@
-import useSWR from 'swr';
+import useSWR from "swr";
 import { VehicleService } from "@/services/vehicle.service";
+import { DateRange } from "react-day-picker";
 
 export function useVehicleTelemetry(id: number) {
-    const service = new VehicleService();
-    const { data, error, isLoading } = useSWR(`vehicle-telemetry-${id}`, () => service.getTelemetry(id), {
-        refreshInterval: 5000,
-    });
+  const service = new VehicleService();
+  const { data, error, isLoading } = useSWR(
+    `vehicle-telemetry-${id}`,
+    () => service.getTelemetry(id),
+    {
+      refreshInterval: 5000,
+    }
+  );
 
-    return {
-        telemetry: data,
-        isLoading,
-        error
-    };
+  return {
+    telemetry: data,
+    isLoading,
+    error,
+  };
 }
 
 export function useVehicleTelemetryHistory(id?: number) {
-    const service = new VehicleService();
-    const { data, error, isLoading } = useSWR(id ? `vehicle-telemetry-history-${id}` : undefined, () => service.getTelemetryHistory(id || 0), {
-        refreshInterval: 5000,
-    });
+  const service = new VehicleService();
+  const { data, error, isLoading } = useSWR(
+    id ? `vehicle-telemetry-history-${id}` : undefined,
+    () => service.getTelemetryHistory(id || 0),
+    {
+      refreshInterval: 5000,
+    }
+  );
 
-    return {
-        telemetryHistory: data,
-        isLoading,
-        error
-    };
+  return {
+    telemetryHistory: data,
+    isLoading,
+    error,
+  };
+}
+
+export function useVehicleTelemetryHistoryFrames(
+  id?: number,
+  dateRange?: DateRange
+) {
+  const service = new VehicleService();
+  const { data, error, isLoading } = useSWR(
+    id ? `vehicle-telemetry-history-frames-${id}` : undefined,
+    () => service.getTelemetryHistory(id || 0)
+  );
+
+  return {
+    telemetryHistory: data,
+    isLoading,
+    error,
+  };
 }
--- src/pages/frame-list/frame-list.tsx
+++ src/pages/frame-list/frame-list.tsx
@@ -1,38 +1,75 @@
 "use client";
 
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { DatePickerWithRange } from "@/components/date-picker";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { useDevices } from "@/hooks/useDevices";
 import { useFrames } from "@/hooks/useFrames";
-import { useVehicleTelemetry, useVehicleTelemetryHistory } from "@/hooks/useVehicleTelemetry";
+import {
+  useVehicleTelemetry,
+  useVehicleTelemetryHistory,
+  useVehicleTelemetryHistoryFrames,
+} from "@/hooks/useVehicleTelemetry";
 import { CameraService } from "@/services/camera.service";
 import { VehicleService } from "@/services/vehicle.service";
 import Image from "next/image";
 import { useState } from "react";
+import { DateRange } from "react-day-picker";
 
 export default function FrameList(props: FrameListProps) {
-    const vehicleService = new VehicleService();
-    const { telemetryHistory } = useVehicleTelemetryHistory(props.vehicleId);
+  const [dateRange, setDateRange] = useState<DateRange>();
+  const vehicleService = new VehicleService();
 
-    return (
-        <section>
-            <header>
-               
-            </header>
-            <main>
-                <section className="grid grid-cols-6">
-                    {
-                        [...telemetryHistory || []].sort((a, b) => parseInt(b.timestamp) > parseInt(a.timestamp) ? 1 : -1).filter(telemetryItem => telemetryItem.objectKey).map(telemetryItem => (
-                            <div key={telemetryItem.objectKey} className="relative h-auto object-fill pb-[100%] overflow-hidden">
-                                <Image key={telemetryItem.objectKey} className="overflow-hidden object-cover" fill={true} src={`https://s3.amazonaws.com/lvb-frames-storage/${telemetryItem.objectKey}`} alt="" />
-                            </div>
-                        ))
-                    }
-                </section>
-            </main>
-        </section>
-    )
+  const { telemetryHistory, isLoading } = useVehicleTelemetryHistoryFrames(
+    props.vehicleId,
+    dateRange
+  );
+
+  function onChangeDate(date: DateRange) {
+    setDateRange(date);
+  }
+
+  return (
+    <section>
+      <header className="mb-8">
+        <DatePickerWithRange onChange={onChangeDate} />
+      </header>
+      <main>
+        {isLoading ? (
+          <>Carregando</>
+        ) : (
+          <section className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
+            {[...(telemetryHistory || [])]
+              .sort((a, b) =>
+                parseInt(b.timestamp) > parseInt(a.timestamp) ? 1 : -1
+              )
+              .filter((telemetryItem) => telemetryItem.objectKey)
+              .map((telemetryItem) => (
+                <div
+                  key={telemetryItem.objectKey}
+                  className="rounded-sm relative h-auto object-fill pb-[100%] overflow-hidden ring-1 ring-border"
+                >
+                  <Image
+                    key={telemetryItem.objectKey}
+                    className="overflow-hidden object-cover"
+                    fill={true}
+                    src={`https://s3.amazonaws.com/lvb-frames-storage/${telemetryItem.objectKey}`}
+                    alt=""
+                  />
+                </div>
+              ))}
+          </section>
+        )}
+      </main>
+    </section>
+  );
 }
 
 type FrameListProps = {
-    vehicleId: number;
-}
+  vehicleId: number;
+};
