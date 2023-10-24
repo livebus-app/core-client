a6e4ea8a52cb4bf15ecc610ee65b4b769e2557df
wolffbruno <wolffbruno72@gmail.com>
2023-10-24 20:03:05 -0300

    refactor(frame-list.tsx): simplify telemetryHistory sorting logic for better readability and performance

--- src/pages/frame-list/frame-list.tsx
+++ src/pages/frame-list/frame-list.tsx
@@ -45,9 +45,7 @@ export default function FrameList(props: FrameListProps) {
         ) : (
           <section className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
             {[...(telemetryHistory || [])]
-              .sort((a, b) =>
-                parseInt(a.timestamp) > parseInt(b.timestamp) ? 1 : -1
-              )
+              .sort((a, b) => b.id - a.id)
               .filter((telemetryItem) => telemetryItem.objectKey)
               .map((telemetryItem) => (
                 <div
