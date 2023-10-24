41df8fe0fe3ab3228a7eaed3aa5462c5eda434af
wolffbruno <wolffbruno72@gmail.com>
2023-10-24 19:56:14 -0300

    fix(frame-list.tsx): reverse telemetryHistory sort order to display items in ascending order based on timestamp

--- src/pages/frame-list/frame-list.tsx
+++ src/pages/frame-list/frame-list.tsx
@@ -46,7 +46,7 @@ export default function FrameList(props: FrameListProps) {
           <section className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
             {[...(telemetryHistory || [])]
               .sort((a, b) =>
-                parseInt(b.timestamp) > parseInt(a.timestamp) ? 1 : -1
+                parseInt(a.timestamp) > parseInt(b.timestamp) ? 1 : -1
               )
               .filter((telemetryItem) => telemetryItem.objectKey)
               .map((telemetryItem) => (
